ad7f557cb788a39bb67f852b37a5edf38b58af7f
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-18 10:46:29 +0100

    Adding blank lines handle in console

--- console.py
+++ console.py
@@ -1,5 +1,54 @@
 #!/usr/bin/python3
+""" This module contains base class for the console """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
-console = cmd.Cmd()
-console.cmdloop(intro="(hbtn) ")
+
+class HBNBCommand(cmd.Cmd):
+    """ Base console class """
+
+    prompt = "(hbtn) "
+    classes = {"BaseModel"}
+
+    def emptyline(self):
+        """Blank lines handling"""
+        pass
+
+    def do_quit(self, arg):
+        """Quit the console"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quit the console using EOF signal"""
+        return True
+
+    def do_create(self, arg):
+        """Creates a new object"""
+        if not arg:
+            print("** class name missing **")
+        if arg not in self.classes:
+            print("** class doesn't exist **")
+        else:
+            myobj = eval(arg)()
+            print(myobj.id)
+            storage.save()
+
+    # def do_show(self, arg):
+    #     """Prints string repr of an instance"""
+    #     args = arg.split()
+    #     if not args:
+    #         print("** class name missing **")
+    #     else if args[0] not in self.classes:
+    #         print("** class doesn't exist **")
+    #     else if not args[1]:
+    #         print("** instance id missing **")
+    #     objects = storage.all()
+    #     for values in objects.values():
+    #         if values.__class__.__name__ == args[0] and values.id == args[1]:
+    #             print(v)
+    #             return
+    #     print("** no instance found **")
+
+console = HBNBCommand()
+console.cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -4,40 +4,46 @@ import uuid
 import datetime
 import models
 
+
 class BaseModel():
-	"""docstring for BaseModel"""
+    """docstring for BaseModel"""
 
-	def __init__(self, *args, **kwargs):
-		"""docstring for init method of BaseModel"""
-		if kwargs:
-			for key in kwargs:
-				if key != "__class__":
-					self.__dict__[key] = kwargs[key]
-			self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
-			self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.datetime.now()
-			self.updated_at = datetime.datetime.now()
-			models.storage.new(self)		
+    def __init__(self, *args, **kwargs):
+        """docstring for init method of BaseModel"""
+        if kwargs:
+            for key in kwargs:
+                if key != "__class__":
+                    self.__dict__[key] = kwargs[key]
+            self.created_at = datetime.datetime.strptime(
+                self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.datetime.strptime(
+                self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
-	def __str__(self):
-		"""docstring for __str__ method of BaseModel"""
+    def __str__(self):
+        """docstring for __str__ method of BaseModel"""
 
-		string = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-		return (string)
+        string = "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+        return (string)
 
-	def save(self):
-		"""saves updated class to storage"""
+    def save(self):
+        """saves updated class to storage"""
 
-		self.updated_at = datetime.datetime.now()
-		models.storage.save()
+        self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
-	def to_dict(self):
-		"""returns a dictionary containing all keys/values of __dict__"""
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
 
-		newdict = dict(self.__dict__)
-		newdict['__class__'] = self.__class__.__name__
-		newdict['created_at'] = str(self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
-		newdict['updated_at'] = str(self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
-		return (newdict)
+        newdict = dict(self.__dict__)
+        newdict['__class__'] = self.__class__.__name__
+        newdict['created_at'] = str(
+            self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        newdict['updated_at'] = str(
+            self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f"))
+        return (newdict)
