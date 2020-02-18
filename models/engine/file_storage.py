04eb26d776fecd9d419dd1b54260f51537474854
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-18 10:36:07 +0100

    Pep8 styled all py files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,43 +4,44 @@
 import json
 import os
 
+
 class FileStorage():
-	"""docstring for FileStorage"""
+    """docstring for FileStorage"""
 
-	def __init__(self):
-		"""docstring for init method of FileStorage"""
+    def __init__(self):
+        """docstring for init method of FileStorage"""
 
-		self.__file_path = "file.json"
-		self.__objects = dict()
+        self.__file_path = "file.json"
+        self.__objects = dict()
 
-	def all(self):
-		""" returns all objects in file """
+    def all(self):
+        """ returns all objects in file """
 
-		return (self.__objects)
+        return (self.__objects)
 
-	def new(self, obj):
-		""" adds new object to dict """
+    def new(self, obj):
+        """ adds new object to dict """
 
-		key = obj.__class__.__name__ + "." + obj.id
-		self.__objects[key] = obj
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
 
-	def save(self):
-		""" serializes __objects to the JSON file """
-		with open(self.__file_path, "w") as file:
-			new_dict = {}
-			for key, value in self.__objects.items():
-				new_dict[key] = value.to_dict()
-			json_text = json.dumps(new_dict)
-			file.write(json_text)
+    def save(self):
+        """ serializes __objects to the JSON file """
+        with open(self.__file_path, "w") as file:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                new_dict[key] = value.to_dict()
+            json_text = json.dumps(new_dict)
+            file.write(json_text)
 
-	def reload(self):
-		""" reloads all objects from file """
+    def reload(self):
+        """ reloads all objects from file """
 
-		from models.base_model import BaseModel
-		if os.path.isfile(self.__file_path):
-			with open(self.__file_path, 'r') as file:
-				json_data = json.load(file)
-			for kwar in json_data.values():
-				cl = kwar["__class__"]
-				cl = eval(cl)
-				self.new(cl(**kwar))
+        from models.base_model import BaseModel
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path, 'r') as file:
+                json_data = json.load(file)
+            for kwar in json_data.values():
+                cl = kwar["__class__"]
+                cl = eval(cl)
+                self.new(cl(**kwar))
