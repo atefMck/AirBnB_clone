9c3136fc4f002929b5b297d624b4969b0a393012
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-19 22:35:30 +0100

    Modifies User class

--- models/user.py
+++ models/user.py
@@ -5,9 +5,8 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """docstring for User class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """docstring for init method of User"""
-        super(User, self).__init__(*args, **kwargs)
         email = ""
         password = ""
         firstname = ""
