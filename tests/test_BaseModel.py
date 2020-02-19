63290e2a90f3a253e09b47732fc369982768b325
cedouiri <cedouiri@gmail.com>
2020-02-19 20:47:30 +0000

    test instance

--- tests/test_BaseModel.py
+++ tests/test_BaseModel.py
@@ -8,20 +8,34 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """ Test cases for the BaseModel """
+    """
+    Test cases for the BaseModel
+    """
     def setUp(self):
         self.base = BaseModel()
 
     def testId(self):
-        """ Test id"""
+        """
+        Test id
+        """
         self.assertEqual(type(self.base.id), str)
 
     def testLen(self):
-        """ Test length of id """
+        """
+        Test length of id
+        """
         self.assertEqual(len(self.base.id), 36)
 
     @unittest.expectedFailure
     def testUnique(self):
-        """ Test if id is unique """
+        """
+        Test if id is unique
+        """
         self.base1 = BaseModel()
         self.assertEqual(self.base.id, self.base1.id)
+
+    def testInstance(self):
+        """
+        Test the variable createdat is an instance of date time
+        """
+        self.assertTrue(isinstance(self.base.created_at, datetime))
