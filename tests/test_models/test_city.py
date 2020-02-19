681b97e139792e4073939a1989aa3852a9eda5b8
cedouiri <cedouiri@gmail.com>
2020-02-19 22:56:14 +0000

    add more test

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+City Class Unittest
+"""
+import unittest
+import models
+from models.city import City
+
+
+class TestCityClass(unittest.TestCase):
+    """
+    testing city class
+    """
+
+    def test_attr(self):
+        """
+        city test
+        """
+        self.one = City()
+        self.assertTrue(hasattr(self.one, "state_id"))
+        self.assertTrue(hasattr(self.one, "name"))
+        self.assertFalse(hasattr(self.one, "area"))
+
+    def test_value(self):
+        """
+        test city value type
+        """
+        self.one = City()
+        self.assertEqual(self.one.state_id, "")
+        self.assertEqual(self.one.name, "")
+        self.assertFalse(self.one.state_id, "CA.94500")
+
+if __name__ == '__main__':
+    unittest.main()
